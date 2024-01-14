d49de5e9d6ff9f63c39708defe55463d3299a1e8
ajlow2000 <ajlow2000@gmail.com>
2024-01-13 17:59:05 -0700

    feat(elora)!: Reorganize overlay with relocated space
    BREAKING CHANGE:

--- elora/qmk/keymap.c
+++ elora/qmk/keymap.c
@@ -52,11 +52,11 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT_myr(
-        TD_CV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             XXXXXXX, XXXXXXX,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    G(KC_L),
-        KC_TAB , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             XXXXXXX, XXXXXXX,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_TAB,
-        CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             XXXXXXX, XXXXXXX,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                   XXXXXXX, KC_LGUI, ALT_ENT, KC_SPC,  L_SYM,   L_NAV,   NUM_SPC, KC_BSPC, KC_DEL,  XXXXXXX,
+        TD_CV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            XXXXXXX, XXXXXXX,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    G(KC_L),
+        KC_TAB , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            XXXXXXX, XXXXXXX,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_TAB,
+        CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            XXXXXXX, XXXXXXX,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   _______, _______, _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                   XXXXXXX, ALT_ENT, KC_SPC, L_SYM,   KC_LGUI, KC_DEL,  L_NAV,   NUM_SPC, KC_BSPC, XXXXXXX,
 
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX
     ),
@@ -95,8 +95,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  _______, _______, _______, _______, _______,          XXXXXXX, XXXXXXX,          _______, _______, _______, _______, _______, KC_BSPC,
         KC_TAB,  _______, _______, _______, _______, _______,          XXXXXXX, XXXXXXX,          _______, _______, _______, _______, _______, KC_ESC,
         KC_LCTL, _______, _______, _______, _______, _______,          XXXXXXX, XXXXXXX,          _______, _______, _______, _______, _______, KC_ENT,
-        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F4,
-                                   XXXXXXX, KC_LCTL, KC_LALT, KC_SPC,  L_SYM,   L_NAV,   NUM_SPC, KC_BSPC, KC_F1,   XXXXXXX,
+        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, KC_F1,   KC_F11,  _______, _______, _______, _______, _______, KC_F4,
+                                   XXXXXXX, KC_LCTL, KC_SPC,  L_SYM,   KC_LALT, KC_F2,   L_NAV,   NUM_SPC, KC_BSPC, XXXXXXX,
 
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX
     ),
